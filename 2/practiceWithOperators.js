517662e18c2c1075d46d2b61f2eca5de840596ec
MadAlexMan <89035276071beeline@gmail.com>
2022-09-03 19:59:33 +0300

    boring

--- 2/practiceWithOperators.js
+++ 2/practiceWithOperators.js
@@ -6,3 +6,26 @@ let condomPerPersons = condoms / humans;
 let fuckedBedsPerPersons = beds/condomPerPersons;
 
 console.log(fuckedBedsPerPersons);
+//-----------------------------------------------------------------------
+
+let vodka = 50;
+let tomato = 120;
+let lemon = 10;
+let tabasco = 1;
+let worcester =1;
+
+let volume = 500;
+
+let ratio = (vodka + tomato + lemon + tabasco + worcester) / volume;
+
+let vodkaForVolume = vodka / ratio;
+let tomatoForVolume = tomato / ratio;
+let lemonForVolume = lemon / ratio;
+let tabascoForVolume = tabasco / ratio;
+let worcesterForVolume = worcester / ratio;
+
+let total = vodkaForVolume + tomatoForVolume + lemonForVolume + tabascoForVolume + worcesterForVolume;
+
+console.log(total);
+//-------------------------------------------------------------------------------
+

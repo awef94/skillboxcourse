f49595b087d687496403f6bc72ef64ab754a64bc
MadAlexMan <89035276071beeline@gmail.com>
2022-09-04 10:12:52 +0300

    shit upd

---
+++ 2/comparison.js
@@ -0,0 +1,22 @@
+let x = 10;
+let y = 20;
+
+console.log(x===y);
+console.log(x===10);
+
+console.log(x!==y);
+console.log(x!==10);
+
+console.log(x<y);
+console.log(x>y);
+console.log(x<=y);
+console.log(x>=y);
+
+/*
+https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Infinity
+*/
+
+/*
+https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/NaN
+*/
+
--- 2/mathFunc.js
+++ 2/mathFunc.js
@@ -1,3 +1,7 @@
 /*
 https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math
 */
+
+Math.random(); //0-1
+Math.round(Math.random()*100); //0-100
+Math.round(Math.random*111)+11; //11-122

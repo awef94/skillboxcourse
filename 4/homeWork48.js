718ad7ae6416b0804562bd5ffd7626c9220c9903
MadAlexMan <89035276071beeline@gmail.com>
2022-09-07 09:33:36 +0300

    HomeWork4

---
+++ 4/homeWork48.js
@@ -0,0 +1,52 @@
+let n = 69;
+let m = 666;
+let array = [];
+
+for(let count = Math.round(Math.random()*(Math.abs(n-m))+Math.min(m,n)); count > 0; --count){
+
+    array.unshift(Math.round(Math.random()*(Math.abs(n-m))+Math.min(m,n)));
+
+}
+console.log(array);
+
+//---------------------------------------------------------------
+
+let strng = "Your mom love my gun!";
+let rvrsStrng = "";
+
+for(let length = strng.length; length >= 0; --length){
+    rvrsStrng+=strng.charAt(length);
+}
+console.log(rvrsStrng);
+
+//----------------------------------------------------------------------
+
+let tank = 3;
+let road =[];
+for(let pos = Math.round(Math.random()*42); pos > 0; --pos){
+    let mine = Math.round(Math.random()*2);
+    if(mine == 1){
+        road.push(true);
+    }else(road.push(false));
+}
+console.log(road);
+
+let roadPOS = 0;
+
+for(let turn of road){
+    console.log(`Tank move to ${++roadPOS}`);
+    if(turn == true){
+        --tank;
+        if (tank>1){
+            console.log("damaged");
+            continue;
+        }
+        else{
+            console.log("KIA");
+            break;
+        }
+    }
+    else{
+        console.log("Tank must go on!");
+    }
+}

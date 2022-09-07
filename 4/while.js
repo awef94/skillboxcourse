9540ce60b08f752b5c629f04e780041cf38d97bb
MadAlexMan <89035276071beeline@gmail.com>
2022-09-07 06:26:51 +0300

    cycles

---
+++ 4/while.js
@@ -0,0 +1,25 @@
+let players = 69;
+
+console.log("Prepare to win!")
+
+while(players >0){
+    if(Math.round(Math.random()*10) == Math.round(Math.random()*10)){
+        players-=1;
+        console.log("Nice one!");
+    }
+    else{
+        console.log("Not now :(");
+    }
+}
+
+console.log("nobody win!");
+
+//----------------------------------------------------
+
+let nums = 69;
+do{
+    console.log("YAY!!!");
+    --nums;
+}while(nums>0);
+
+console.log("success");

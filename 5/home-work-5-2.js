ab7f4dbc0657865ac1c9c9a32f147c82961a5b16
MadAlexMan <89035276071beeline@gmail.com>
2022-09-09 19:57:42 +0300

    fixprice

--- 5/home-work-5-2.js
+++ 5/home-work-5-2.js
@@ -1,9 +1,8 @@
 function calcTotal(sum, quantity, promo = null) {
 
     if(sum > 50000){
-        let sale = Math.abs(50000 - sum);
-        let promoSale = sale*0.2
-        sum = 50000+promoSale;
+        let dif = Math.abs(50000 - sum)*0.2;
+        sum = sum-dif;
     }
 
     if(promo == `sale15`){
@@ -33,5 +32,10 @@ console.log(calcTotal(100, 15));
 console.log(calcTotal(1000, 11, `gift300`));
 console.log(calcTotal(150,5,`gift300`));
 console.log(calcTotal(350,2,`gift300`));
-
+console.log(calcTotal(21000, 9, `sale15`));
+console.log(calcTotal(70000, 9));
+console.log(calcTotal(55000, 9));
+console.log(calcTotal(80000, 9));
+console.log(calcTotal(90000, 9));
+console.log(calcTotal(99999, 9));
 

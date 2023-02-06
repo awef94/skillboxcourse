8994d123378ac74c78f289c7fbc065172176dfa0
MadAlexMan <89035276071beeline@gmail.com>
2023-02-06 20:54:18 +0300

    timer done! fix timing and style

--- 7/timer/back.js
+++ 7/timer/back.js
@@ -18,22 +18,30 @@ document.addEventListener(`DOMContentLoaded`,
                 count.style.backgroundColor = `red`;
             }
         }
-
         function timer() {
             let counter = parseInt(count.textContent);
-            if(counter<=0){
+            if(counter<=1){
                 clearInterval(startStop);
                 audio.play();
+                count.textContent = `done!`;
             }else{
                 console.log(counter--);
                 count.textContent = counter;
             }
         }
-
         function drop() {
             clearInterval(startStop);
         }
+        
+        function animButton1() {
+            button.style.border = `10px outset whitesmoke`;
+        }
+        function animButton2() {
+            button.style.border = `10px inset whitesmoke`;
+        }
 
+        button.addEventListener(`mouseup`, animButton1)
+        button.addEventListener(`mousedown`, animButton2);
         button.addEventListener(`click`, drop);
         button.addEventListener(`click`, check);
 
